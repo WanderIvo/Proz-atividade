dab64c26849389270a6dcff624894fba263a9a52
wandinhobjjj1 <168235148+wandinhobjjj1@users.noreply.github.com>
2024-04-26 18:59:58 -0300

    primeiro commit

---
+++ ex1.py
@@ -0,0 +1,5 @@
+contador = 20
+while contador > 1:
+    if contador != 18:
+      print(contador)
+    contador -= 1
---
+++ ex2.py
@@ -0,0 +1,30 @@
+"""
+Var
+// Seção de Declarações das variáveis 
+   contador:inteiro
+   
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+  contador <- 20
+  enquanto contador >1 faca
+  se contador <> 18 entao
+  escreva(contador)
+  fimse
+  contador <- contador -1
+  fimenquanto
+
+Fimalgoritmo
+"""
+
+
+contador = 20
+while contador > 1:
+    if contador != 18:
+        print(contador)
+    contador -= 1
+
+
+
+
+
