90acad5f341be8ead229cdde2e8a82bc7354dd6d
wandinhobjjj1 <168235148+wandinhobjjj1@users.noreply.github.com>
2024-04-27 20:06:44 -0300

    Segundo Projeto em Python

---
+++ app.py
@@ -0,0 +1,13 @@
+def calculadora (numero1,  numero2 ,resultado):
+    if resultado == 1:
+        return numero1 + numero2
+    elif resultado == 2:
+        return numero1 + numero2
+    elif resultado == 3:
+        return numero1 * numero2
+    elif resultado == 4:
+        return numero1 / numero2
+    else: 
+        return  0
+n1 = calculadora (3, 10, 4)
+print('valor de n1 e', n1) 
